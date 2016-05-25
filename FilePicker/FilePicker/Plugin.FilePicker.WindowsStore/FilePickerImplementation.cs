51edfbd72a02e18d39cb57200193749b4b96a775
Rafael Moura <rafaelmoura-fidelity@outlook.com>
2016-05-24 23:14:21 -0300

    Nuspec & NotImplementException for 8, 8.1 s WS

--- FilePicker/FilePicker/Plugin.FilePicker.WindowsStore/FilePickerImplementation.cs
+++ FilePicker/FilePicker/Plugin.FilePicker.WindowsStore/FilePickerImplementation.cs
@@ -1,13 +1,32 @@
 using Plugin.FilePicker.Abstractions;
 using System;
-
+using System.Threading.Tasks;
 
 namespace Plugin.FilePicker
 {
-  /// <summary>
-  /// Implementation for FilePicker
-  /// </summary>
-  public class FilePickerImplementation : IFilePicker
-  {
-  }
+    /// <summary>
+    /// Implementation for FilePicker
+    /// </summary>
+    public class FilePickerImplementation : IFilePicker
+    {
+        public void OpenFile(FileData fileToOpen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenFile(string fileToOpen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileData> PickFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveFile(FileData fileToSave)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
