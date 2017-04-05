c6e2373528bb2d696f85dee61ff469bca957e57f
Drew Frisk <hello@drewfrisk.com>
2017-04-04 22:10:18 -0500

    cleaned up implementation

--- FilePicker/FilePicker/Plugin.FilePicker.Mac/FilePickerImplementation.cs
+++ FilePicker/FilePicker/Plugin.FilePicker.Mac/FilePickerImplementation.cs
@@ -12,70 +12,30 @@ namespace Plugin.FilePicker
 {
     public class FilePickerImplementation : NSObject, IFilePicker
     {
-        private int _requestId;
-        private TaskCompletionSource<FileData> _completionSource;
-
-        /// <summary>
-        /// Event which is invoked when a file was picked
-        /// </summary>
-        public EventHandler<FilePickerEventArgs> Handler
+        public Task<FileData> PickFile()
         {
-            get;
-            set;
-        }
-
-        public async Task<FileData> PickFile()
-        {
-            var media = await TakeMediaAsync();
-
-            return media;
-        }
-
-        private Task<FileData> TakeMediaAsync()
-        {
-            var id = GetRequestId();
-
-            var ntcs = new TaskCompletionSource<FileData>(id);
-
-            if (Interlocked.CompareExchange(ref _completionSource, ntcs, null) != null)
-                throw new InvalidOperationException("Only one operation can be active at a time");
-
-            var allowedUtis = new string[] {
-                UTType.UTF8PlainText,
-                UTType.PlainText,
-                UTType.RTF,
-                UTType.PNG,
-                UTType.Text,
-                UTType.PDF,
-                UTType.Image,
-                UTType.UTF16PlainText,
-                UTType.FileURL
-            };
-
-            var dlg = NSOpenPanel.OpenPanel;
-            dlg.CanChooseFiles = true;
-            dlg.CanChooseDirectories = false;
-            //dlg.AllowedFileTypes = new string[] { "txt", "html", "md", "css" };
+            // for consistency with other platforms, only allow selecting of a single file.
+            // would be nice if we passed a "file options" to override picking multiple files & directories
+            var openPanel = new NSOpenPanel(); ;
+            openPanel.CanChooseFiles = true;
+            openPanel.AllowsMultipleSelection = false;
+            openPanel.CanChooseDirectories = false;
 
             FileData data = null;
 
-            //await Task.Factory.StartNew(() =>
-            //{
-            var result = dlg.RunModal();
+            var result = openPanel.RunModal();
             if (result == 1)
             {
                 // Nab the first file
-                var url = dlg.Urls[0];
-                var fileName = dlg.Filenames[0];
+                var url = openPanel.Urls[0];
+                var fileName = openPanel.Filenames[0];
 
                 if (url != null)
                 {
                     var path = url.Path;
-                    //url.fr
                     data = new FileData(path, fileName, () => File.OpenRead(path));
                 }
             }
-            //});
 
             return Task.FromResult(data);
         }
@@ -86,17 +46,15 @@ namespace Plugin.FilePicker
             {
                 var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-                var dlg = NSSavePanel.SavePanel;
-                dlg.Title = $"Save {fileToSave.FileName}";
-                dlg.CanCreateDirectories = true;
-                //dlg.ShouldShowFilename = 
-                //dlg.Filename = fileToSave.FileName;
+                var savePanel = new NSSavePanel();
+                savePanel.Title = $"Save {fileToSave.FileName}";
+                savePanel.CanCreateDirectories = true;
 
-                var result = dlg.RunModal(documents, fileToSave.FileName);
+                var result = savePanel.RunModal(documents, fileToSave.FileName);
 
                 if (result == 1)
                 {
-                    var path = dlg.Url.Path;
+                    var path = savePanel.Url.Path;
 
                     File.WriteAllBytes(path, fileToSave.DataArray);
 
@@ -114,36 +72,40 @@ namespace Plugin.FilePicker
 
         public void OpenFile(string fileToOpen)
         {
-            if (!NSWorkspace.SharedWorkspace.OpenFile(fileToOpen))
+            try
             {
-                Debug.WriteLine($"Unable to open file \"{fileToOpen}\"");
+                if (!NSWorkspace.SharedWorkspace.OpenFile(fileToOpen))
+                {
+                    Debug.WriteLine($"Unable to open file at path: {fileToOpen}.");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+            }
+            catch (Exception ex)
+            {
             }
         }
 
-        public void OpenFile(FileData fileToOpen)
+        public async void OpenFile(FileData fileToOpen)
         {
-            string fileUrl = fileToOpen.FileName;
-
-            if (fileToOpen.FilePath != null &&
-               !fileToOpen.FilePath.EndsWith(fileToOpen.FileName, StringComparison.CurrentCulture) &&
-               !fileToOpen.FilePath.Equals(fileToOpen.FileName))
+            try
             {
-                fileUrl = string.Format("{0}/{1}", fileToOpen.FilePath, fileToOpen.FileName);
+                if (!NSWorkspace.SharedWorkspace.OpenFile(fileToOpen.FileName))
+                {
+                    Debug.WriteLine($"Unable to open file at path: {fileToOpen}.");
+                }
             }
-
-            OpenFile(fileToOpen.FileName);
-        }
-
-        private int GetRequestId()
-        {
-            var id = _requestId;
-
-            if (_requestId == int.MaxValue)
-                _requestId = 0;
-            else
-                _requestId++;
-
-            return id;
+            catch (FileNotFoundException ex)
+            {
+                // this could be some strange UI behavior.
+                // user would get prompted to save the file in order to open the file
+                await SaveFile(fileToOpen);
+                OpenFile(fileToOpen);
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
