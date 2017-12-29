89e41e0b56c8cce3774dc55cf2105c7b81d84cdf
Gerald Versluis <gerald@verslu.is>
2017-12-29 01:39:16 +0100

    Added Droid and iOS Xamarin.Forms sample projects and implemented Droid

---
+++ samples/Plugin.FilePicker.Sample.Forms/Plugin.FilePicker.Sample.Forms/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace Plugin.FilePicker.Sample.Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Plugin_FilePicker_Sample_FormsPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ samples/Plugin.FilePicker.Sample.Forms/Plugin.FilePicker.Sample.Forms/Plugin.FilePicker.Sample.FormsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Plugin.FilePicker.Sample.Forms
+{
+    public partial class Plugin_FilePicker_Sample_FormsPage : ContentPage
+    {
+        public Plugin_FilePicker_Sample_FormsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Handle_Clicked(object sender, EventArgs args)
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                await DisplayAlert("TODO", "Whoops, for iOS the sample is to be implemented", "OK");
+
+                return;
+            }
+
+            var pickedFile = await CrossFilePicker.Current.PickFile();
+
+            FileNameLabel.Text = pickedFile.FileName;
+            FilePathLabel.Text = pickedFile.FilePath;
+
+            if (pickedFile.FileName.EndsWith("jpg", StringComparison.Ordinal)
+                || pickedFile.FileName.EndsWith("png", StringComparison.Ordinal))
+            {
+                FileImagePreview.Source = ImageSource.FromStream(() => pickedFile.GetStream());
+                FileImagePreview.IsVisible = true;
+            }
+            else
+            {
+                FileImagePreview.IsVisible = false;
+            }
+        }
+    }
+}
