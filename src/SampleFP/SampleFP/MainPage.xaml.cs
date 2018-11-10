5b5bf1de3f00ba481add4b44388504d4f32a2b1c
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-11-10 21:11:17 -0200

    AllChanges

---
+++ src/SampleFP/SampleFP/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Plugin.FilePicker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleFP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            FileData fileData = await FilePicker.PickFile();
+            var path = @"C:\Users\pedro\source\repos\pictos\FilePicker-Plugin-for-Xamarin-and-Windows\.gitignore";
+            FilePicker.OpenFile(path);
+        }
+    }
+}
