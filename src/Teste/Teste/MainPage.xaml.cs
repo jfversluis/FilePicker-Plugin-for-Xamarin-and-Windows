e2d163cad9286468b31d0a3c010be196de8ac6f5
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-11-10 21:29:17 -0200

    Add reference to all projects

---
+++ src/Teste/Teste/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Teste
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var file = await Plugin.FilePicker.CrossFilePicker.Current.PickFile();
+        }
+    }
+}
