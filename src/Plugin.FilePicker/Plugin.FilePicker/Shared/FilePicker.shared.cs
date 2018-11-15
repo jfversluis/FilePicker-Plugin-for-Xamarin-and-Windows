19d005e5a3013d2cc92e0b219bc93a089e839729
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-11-15 18:34:37 -0200

    Change CrossFilePicker to FilePicker; Create a FilePicker.uwp.cs

--- src/Plugin.FilePicker/Plugin.FilePicker/Shared/FilePicker.shared.cs
+++ src/Plugin.FilePicker/Plugin.FilePicker/Shared/FilePicker.shared.cs
@@ -2,7 +2,7 @@
 
 namespace Plugin.FilePicker
 {
-    public static class CrossFilePicker
+    public static class FilePicker
     {
         public static Task<FileData> PickFile(string[] allowedTypes = null) => PlataformPickFile(allowedTypes);
 
---
+++ src/Plugin.FilePicker/Plugin.FilePicker/UWP/FilePicker.uwp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.FilePicker
+{
+    static partial class FilePicker
+    {
+    }
+}
