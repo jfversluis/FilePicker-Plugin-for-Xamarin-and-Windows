5d12296df8dee2a574e98a203242a5bdca9dd3d2
Michael Fink <michael.fink@asamnet.de>
2020-02-27 20:34:04 +0100

    removed and sorted usings in the sample projects

--- samples/Plugin.FilePicker.Sample.Forms/Droid/MainActivity.cs
+++ samples/Plugin.FilePicker.Sample.Forms/Droid/MainActivity.cs
@@ -1,11 +1,5 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Plugin.FilePicker.Sample.Forms.Droid
@@ -20,7 +14,7 @@ namespace Plugin.FilePicker.Sample.Forms.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App());
         }
