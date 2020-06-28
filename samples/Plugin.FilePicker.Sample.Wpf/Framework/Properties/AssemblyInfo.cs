696bcc1b21c2049799420542bf4e143854ffcf35
Jaap de Wolff <jaap@de-wolff.org>
2020-06-28 21:14:33 +0200

    Add support for dotnet core > 3.0, dotnet 46, 47, 48 (#188)
    * Add support for dotnet core > 3.0, dotnet 46, 47, 48
    
    * Sample for all WPF framework versions
    
    Co-authored-by: Gerald Versluis <github@geraldversluis.nl>

---
+++ samples/Plugin.FilePicker.Sample.Wpf/Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
---
+++ samples/Plugin.FilePicker.Sample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Plugin.FilePicker.Sample.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string File { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            DataContext = this;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var data = await FilePicker.CrossFilePicker.Current.PickFile();
+            File = data.FilePath;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("File"));
+        }
+    }
+}
